e090a46f923cdb4ec2c0d78c3668f55dbb9a08b7
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-15 01:41:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
 
-function App() {
+import { SearchBar, VideoList, VideoDetail } from "./components";
+
+import youtube from "./api/youtube";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
